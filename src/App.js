f75e256ce8369529ed95be1070de6f332d0cd5a4
Rachel Siepelinga <rsiepelinga@gmail.com>
2022-11-12 15:28:06 -0500

    updating layout and app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import AppRoutes from './AppRoutes.js';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { appTheme } from './themes/theme.js';
 
 export default function App() {
   return (
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline enableColorScheme />
-      <AppRoutes/>
-    </ThemeProvider>
+    <AppRoutes/>
   );
 }
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import NavBar from '../components/navigation/NavBar.js';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { guestTheme } from '../themes/guestTheme.js';
+import { appTheme } from '../themes/theme.js';
 
 const Layout = ({component: Component}) => {
   
   return (
-    <div>
+    <ThemeProvider theme={guestTheme}>
+      <CssBaseline enableColorScheme />
       <NavBar/>
       <div style={{padding: "80px 20px 0px"}}>
         <Component/>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
