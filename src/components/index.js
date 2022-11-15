93002cc32bc93315bb2626d1d8d562750cdb4839
Rachel Siepelinga <rsiepelinga@gmail.com>
2022-11-15 18:54:12 -0500

    File structure update

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Carousel } from './games/Carousel';
+export { default as Library } from './games/Library';
+export { default as NavBar } from './navigation/NavBar';
--- src/views/group/GroupDetail.js
+++ src/views/group/GroupDetail.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import { Typography, Container } from '@mui/material';
-import Carousel from '../components/games/Carousel.js';
+import { Carousel } from '../../components';
 
 const GroupDetail = (props) => {
-  var style = {
-    h1: {
-      color: '#FFF',
-      textAlign: 'center',
-      fontSize: '24px',
-      fontWeight: 'bold'
-    },
-    p: {
-      color: '#FFF',
-      textAlign: 'center',
-      fontSize: '18px'
-    }
-  }
-
   var collection = [
     {
       id: "31260",
