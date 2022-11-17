ac879b7807d6fcb4e0cd00e2d0b29a28838f186e
Rachel Siepelinga <rsiepelinga@gmail.com>
2022-11-16 21:28:01 -0500

    updating indenting

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use('/server/collection', require('./routes/server/collection'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!')
+})
 
 app.listen(8000, () => console.log('Server Running'));
 
