395431cda74c6979111494953af0b3bbbda0224f
Rachel Siepelinga <rsiepelinga@gmail.com>
2022-11-16 21:27:46 -0500

    Fixing indenting

--- server/routes/server/collection.js
+++ server/routes/server/collection.js
@@ -4,45 +4,45 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Info');
+  res.send('Info');
 });
 
 router.get('/username/:userId', (req, res) => {
-    getCollection(req.params.userId).then((result) => res.send(result));
+  getCollection(req.params.userId).then((result) => res.send(result));
 })
 
 router.get('/usernames', (req, res) => {
-    const usernameString = req.query.names;
-    let usernames = usernameString.split(';');
-
-    let promises = [];
-    usernames.forEach((name) => {
-        promises.push(getCollection(name));
-    });
-
-    Promise.all(promises).then((results) => {
-        res.send(results);
-    })
+  const usernameString = req.query.names;
+  let usernames = usernameString.split(';');
+  
+  let promises = [];
+  usernames.forEach((name) => {
+    promises.push(getCollection(name));
+  });
+  
+  Promise.all(promises).then((results) => {
+    res.send(results);
+  })
 });
 
 async function getCollection(name) {
-    let res = await bgg.getBggCollection({username: name, own: 1, stats: 1});
-    if(res.status !== 200) {
-        return res.statusText;
-    }
-    if(res.data.error) {
-        return res.data.error.message;
-    }
-    if(res.data.totalitems === '0') {
-        return [];
-    }
-
-    let arr = [];
-    res.data.item.forEach(element => {
-        let c = new Collection(element, name);
-        arr.push(c);
-    });
-    return arr;
+  let res = await bgg.getBggCollection({username: name, own: 1, stats: 1});
+  if(res.status !== 200) {
+    return res.statusText;
+  }
+  if(res.data.error) {
+    return res.data.error.message;
+  }
+  if(res.data.totalitems === '0') {
+    return [];
+  }
+  
+  let arr = [];
+  res.data.item.forEach(element => {
+    let c = new Collection(element, name);
+    arr.push(c);
+  });
+  return arr;
 }
 
 module.exports = router;
