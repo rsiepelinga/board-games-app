9b62c854b11e20344ab5a38829a8d300bdb69d3d
Rachel Siepelinga <rsiepelinga@gmail.com>
2022-11-16 22:57:22 -0500

    enabled cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 
 app.use('/api/collection', require('./routes/api/collection'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(8000, () => console.log('Server Running'));
